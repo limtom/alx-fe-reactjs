24ac126caf286546ecdf46dd0dfd02799438436f
limtom <horlartom@live.com>
2026-01-18 19:27:39 +0100

    Task-3

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src="./vite.svg" alt="Vite-logo" />
+      <ul className="navbar-link">
+        <NavLink to="/">
+          <li>Home</li>
+        </NavLink>
+        <NavLink to="about">
+          <li>About</li>
+        </NavLink>
+        <NavLink to="services">
+          <li>Services</li>
+        </NavLink>
+        <NavLink to="contact">
+          <li>Contact</li>
+        </NavLink>
+      </ul>
+    </nav>
+  );
+}
+export default Navbar;
