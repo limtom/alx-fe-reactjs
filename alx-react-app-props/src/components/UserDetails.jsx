b9df59a9ef69c3cb6d25c547aed38694355eaa21
limtom <horlartom@live.com>
2026-01-18 18:13:48 +0100

    Task-2

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { userContext } from "../UserContext";
+import { userContext } from "./UserContext";
 
 function UserDetails() {
   const userData = useContext(userContext);
