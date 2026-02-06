c637f3ad57ad4ee72b2b974e00b62a10e73bc781
limtom <horlartom@live.com>
2026-02-06 10:56:32 +0100

    Task-2: Responsiveness-implement

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,13 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="user-profile bg-gray-100 p-4 mx-auto my-20 max-w-xs rounded-lg shadow-lg md:p-8 md:max-w-sm ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto"
+        className="rounded-full w-24 h-24 md:w-36 md:h-36 mx-auto"
       />
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">
+      <h1 className="text-lg text-blue-800 my-4 md:text-xl">John Doe</h1>
+      <p className="text-gray-600 text-sm md:text-base">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
