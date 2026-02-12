e8b56b7ecdb879c9b3cf1725e4ee37fe946e767c
limtom <horlartom@live.com>
2026-02-12 14:56:17 +0100

    Initial setup

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,30 +1,5 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
-
-import RootLayout from "./Layout/RootLayout";
-import RecipeList from "../src/components/RecipeList";
-import Home from "../src/components/Home";
-import RecipeDetails from "./components/RecipeDetails";
-import RecipeLayout from "./layout/RecipeLayout";
-
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route path="recipes" element={<RecipeLayout />}>
-          <Route index element={<RecipeList />} />
-          <Route path=":id" element={<RecipeDetails />} />
-        </Route>
-      </Route>,
-    ),
-  );
-
-  return <RouterProvider router={router} />;
+  return <div>Hello world</div>;
 }
 
 export default App;
