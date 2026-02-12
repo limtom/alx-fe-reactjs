ed133693235d0d2fe6612039950b89ec300876fe
limtom <horlartom@live.com>
2026-02-12 22:52:17 +0100

    Homepage and routes implemented

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,23 +1,25 @@
 import {
   createBrowserRouter,
   createRoutesFromElements,
+  Routes,
   Route,
-  RouterProvider,
+  BrowserRouter as Router,
 } from "react-router-dom";
 import Homepage from "./components/Homepage";
 import RootLayout from "./components/RootLayout";
 import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Homepage />} />
-        <Route path=":id" element={<RecipeDetail />} />
-      </Route>,
-    ),
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path=":id" element={<RecipeDetail />} />
+        </Route>
+      </Routes>
+    </Router>
   );
-  return <RouterProvider router={router} />;
 }
 
 export default App;
